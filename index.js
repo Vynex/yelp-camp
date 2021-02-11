a0dc3e8310d5c675653f23d253130ac97d9620cf
BuildTools <unconfigured@null.spigotmc.org>
2021-02-11 12:03:18 +0530

    Added Basic CRUD Functionality for Campgrounds

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ mongoose.connection.once('open', () => {
 });
 
 const methodOverride = require('method-override');
+const Campground = require('./models/campground');
 
 
 app.set('view engine', 'ejs');
@@ -27,12 +28,46 @@ app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
+
+    res.redirect('/campgrounds');
     res.render('home');
 });
 
 
+app.get('/campgrounds', async (req, res) => {
+    const camps = await Campground.find({});
 
+    res.render('campgrounds/index', { camps });
+});
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+
+    res.render('campgrounds/view', { id, camp });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const camp = await Campground.findById(id);
+
+    res.render('campgrounds/edit', { id, camp });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, location } = req.body;
+    await Campground.findByIdAndUpdate(id, { title, location });
+
+    res.redirect(`/campgrounds/${id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+
+    res.redirect('/campgrounds');
+})
 
 app.listen(port, () => {
     console.log(`Server Listening at Port ${port}`);
